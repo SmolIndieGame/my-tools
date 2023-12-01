71216decd88dbd9953ef9460e4f63ea15b45b0b5
SmolIndieGame <smolindiegame@gmail.com>
2023-12-01 12:23:19 +0800

    add tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,10 +6,29 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+    vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    test('Sample test', () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
+
+    var array = new Array<number>(100000);
+    test('array shift', () => {
+        const start = Date.now();
+        for (let i = 0; i < 100000; i++) {
+            array.unshift(i);
+            array.pop();
+        }
+        console.log(Date.now() - start);
+    });
+
+    test('array push', () => {
+        const start = Date.now();
+        for (let i = 0; i < 100000; i++) {
+            array.push(i);
+            array.pop();
+        }
+        console.log(Date.now() - start);
+    });
 });
